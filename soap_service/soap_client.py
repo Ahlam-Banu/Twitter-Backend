27c9873b2ea3c3e07eb4ac37dce6df8bd9725e51
Ahlam-Banu <ahlambanu.univ@gmail.com>
2024-03-28 12:38:33 +0200

    soap service client added

---
+++ soap_service/soap_client.py
@@ -0,0 +1,12 @@
+import logging
+from zeep import Client
+
+# Set up logging
+#logging.basicConfig(level=logging.DEBUG)
+
+wsdl_url = 'http://localhost:8000/?wsdl'
+client = Client(wsdl_url)
+
+text_to_translate = "puhun suomea ja englantia samanaikaisesti  ja ymmärrän molempia kieliä."
+result = client.service.translate_text(text_to_translate)
+print("Translated Text:", result)
