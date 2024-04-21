37d69525b52507de9d12127d0ca1d5c35cf07d5d
Nguyen An Phuong Linh <102663928+Flinnefffff@users.noreply.github.com>
2024-04-22 00:37:44 +0300

    Delete Comment_ms/config.py

--- Comment_ms/config.py
+++
@@ -1,9 +0,0 @@
-# import os
-
-# # Define your database connection string for MariaDB
-# DB_USERNAME = 'e2101065'
-# DB_PASSWORD = 'DZCtWC5pEC2'
-# DB_HOST = 'mariadb.vamk.fi'
-# DB_NAME = 'e2101065_comment'
-# SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-# SQLALCHEMY_TRACK_MODIFICATIONS = False
