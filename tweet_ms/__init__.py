f1aa70bf6339a2332bc30e4a7cf9d8298f8af8ea
YuanLai021031 <80317133+YuanLai021031@users.noreply.github.com>
2024-04-24 13:56:45 +0300

    Delete tweet_ms directory

--- tweet_ms/__init__.py
+++
@@ -1,8 +0,0 @@
-# tweet_ms/__init__.py
-from flask import Flask
-
-# Create a Flask application instance
-app = Flask(__name__)
-
-# Import routes from tweet_routes module
-from tweet_ms import tweet_routes
